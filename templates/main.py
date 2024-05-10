25af01c7bd265bff89b817dbe05c2bc592ac87d3
Ishuman Agarwal <mann.agrawal17@gmail.com>
2024-05-10 21:45:19 +0530

    chore: Update config and params files for data ingestion pipeline

--- templates/main.py
+++ templates/main.py
@@ -1,5 +1,15 @@
 
-# from cnnClassifier import logger
+from cnnClassifier import logger
+
+from cnnClassifier.pipeline.stage_01_data_pipeline import DataIngestionTrainingPipeline
 
 
-# logger.info('testing 134')
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    logger.info(f">>>>>>>>> Stage {STAGE_NAME} Started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>> Stage {STAGE_NAME} Completed <<<<<<<<<<\n\n<=========x')
+except Exception as e:
+    raise e
